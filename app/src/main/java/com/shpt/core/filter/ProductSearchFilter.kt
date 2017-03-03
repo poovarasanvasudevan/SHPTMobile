2bdf0971ee1e629f1fc058bcf63d68c2f7f32c55
poovarasan <poosan9@gmail.com>
2017-03-03 19:05:15 +0530

    New Product search changes

--- app/src/main/java/com/shpt/core/filter/ProductSearchFilter.kt
+++ app/src/main/java/com/shpt/core/filter/ProductSearchFilter.kt
@@ -1,91 +1,44 @@
 package com.shpt.core.filter
 
 import android.content.Context
-import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Filter
 import android.widget.Filterable
+import android.widget.TextView
 import com.google.gson.JsonArray
+import com.mcxiaoke.koi.ext.find
+import com.mcxiaoke.koi.ext.inflate
+import com.shpt.R
 import com.shpt.core.api.rest
 import com.shpt.core.config.Config
 import com.shpt.core.models.ProductSearch
 import com.shpt.core.parser
+import com.shpt.core.splitQuery
 import logMessage
-import org.jetbrains.anko.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 /**
  * Created by poovarasanv on 3/3/17.
-
  * @author poovarasanv
- * *
  * @project SHPT
- * *
  * @on 3/3/17 at 4:06 PM
  */
 
-class ProductSearchFilter : ArrayAdapter<ProductSearch>, Filterable {
-    lateinit var mProducts: List<ProductSearch>
-
-    constructor(context: Context, @LayoutRes resource: Int) : super(context, resource) {
-        init()
-    }
-
-    constructor(context: Context, @LayoutRes resource: Int, @IdRes textViewResourceId: Int) : super(context, resource, textViewResourceId) {
-        init()
-    }
-
-    constructor(context: Context, @LayoutRes resource: Int, objects: Array<ProductSearch>) : super(context, resource, objects) {
-        init()
-    }
-
-    constructor(context: Context, @LayoutRes resource: Int, @IdRes textViewResourceId: Int, objects: Array<ProductSearch>) : super(context, resource, textViewResourceId, objects) {
-        init()
-    }
-
-    constructor(context: Context, @LayoutRes resource: Int, objects: List<ProductSearch>) : super(context, resource, objects) {
-        init()
-    }
-
-    constructor(context: Context, @LayoutRes resource: Int, @IdRes textViewResourceId: Int, objects: List<ProductSearch>) : super(context, resource, textViewResourceId, objects) {
-
-        init()
-    }
-
-    fun init() {
-        mProducts = mutableListOf<ProductSearch>()
-    }
-
-    override fun getFilter(): Filter {
-
-        val mFilter = MyFilter()
-
-
-
-        return mFilter;
-    }
-
-    override fun getCount(): Int {
-        return mProducts.size
-    }
-
-    override fun getItem(position: Int): ProductSearch {
-        return mProducts[position]
-    }
-
-
+class ProductSearchFilter(context: Context, @LayoutRes resource: Int) : ArrayAdapter<ProductSearch>(context, resource), Filterable {
+    var mProducts: List<ProductSearch> = mutableListOf<ProductSearch>()
+    override fun getFilter(): Filter = MyFilter()
+    override fun getCount(): Int = mProducts.size
+    override fun getItem(position: Int): ProductSearch = mProducts[position]
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = context.linearLayout {
-            textView {
-                text = mProducts[position].productName
-            }
-            padding = dip(5)
-        }
 
+        val view = context.inflate(R.layout.product_search, parent!!,false)
+        view.find<TextView>(R.id.productName).text = mProducts[position].productName
         return view
-
     }
 
 
@@ -94,19 +47,22 @@ class ProductSearchFilter : ArrayAdapter<ProductSearch>, Filterable {
             val filterResults = Filter.FilterResults()
 
             if (constraint != null && !constraint.isBlank() && constraint.length > 2) {
-                var term = constraint.toString()
+                val term = constraint.toString()
 
                 doAsync {
                     val result: JsonArray = parser.parse(context.rest.getProductSearch(Config.SEARCH_PRODUCT, term).execute().body().string()).asJsonArray
 
                     uiThread {
 
+                        result.forEach {
+                            val productId: String? = splitQuery(URL(it.asJsonObject.get("href").asString.replace("amp;", "")))["product_id"]
 
-//                        result.forEach {
-//                            val productId: Int = splitQuery(URL(it.asJsonObject.get("href").asString))["product_id"]!!.toInt()
-//                            val productSearch = ProductSearch(0, it.asJsonObject.get("name").asString)
-//                            mProducts.toMutableList().add(productSearch)
-//                        }
+                            if (productId != null) {
+                                val productSearch = ProductSearch(productId.toInt(), it.asJsonObject.get("name").asString)
+                                mProducts.toMutableList().add(productSearch)
+                            }
+
+                        }
                         logMessage(result.toString())
 
                         filterResults.count = result.count()
@@ -120,11 +76,12 @@ class ProductSearchFilter : ArrayAdapter<ProductSearch>, Filterable {
 
         override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
             if (results != null && results.count > 0) {
-                notifyDataSetChanged();
+                notifyDataSetChanged()
             } else {
-                notifyDataSetInvalidated();
+                notifyDataSetInvalidated()
             }
         }
 
     }
+
 }
