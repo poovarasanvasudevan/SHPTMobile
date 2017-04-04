f011876e2d36d601a2b550eb9e3ab66b27965178
poovarasan <poosan9@gmail.com>
2017-04-04 19:21:43 +0530

    JobManager replaced need to replace GCMManager import play service lib

--- app/src/main/java/com/shpt/activity/Login.kt
+++ app/src/main/java/com/shpt/activity/Login.kt
@@ -1,6 +1,5 @@
 package com.shpt.activity
 
-import android.content.ComponentName
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,7 +10,6 @@ import com.mcxiaoke.koi.ext.find
 import com.mcxiaoke.koi.ext.toast
 import com.shpt.R
 import com.shpt.core.app.BaseActivity
-import com.shpt.core.config.JOB_SCHEDULER
 import com.shpt.core.config.LAYOUT_BUILDER_FACTORY
 import com.shpt.core.config.PARSER
 import com.shpt.core.config.STYLES
@@ -19,11 +17,9 @@ import com.shpt.core.getLayout
 import com.shpt.core.models.Layout
 import com.shpt.core.serviceevent.RetryServiceEvent
 import com.shpt.core.setUpEssential
-import com.shpt.job.SHPTJobService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
-import me.tatarka.support.job.JobInfo
 import org.greenrobot.eventbus.Subscribe
 import org.jetbrains.anko.coroutines.experimental.bg
 
@@ -33,13 +29,7 @@ class Login : BaseActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		
-		val job = JobInfo.Builder(123, ComponentName(applicationContext, SHPTJobService::class.java))
-			.setPeriodic(2000)
-			.setPersisted(true)
-		
-		JOB_SCHEDULER.schedule(job.build())
-		
+
 		
 		try {
 			
@@ -59,8 +49,7 @@ class Login : BaseActivity() {
 				val view = layoutBuilder.build(
 					mainLayout,
 					PARSER.parse(jsonLayout.structure).asJsonObject.getAsJsonObject("main"),
-					JsonObject()
-					,
+					JsonObject(),
 					0,
 					STYLES.await())
 				
